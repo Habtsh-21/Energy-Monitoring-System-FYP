c66a141c3a53f004f812ea42f660eb27ed2f28fb
Habtsh-21 <hworku901@gmail.com>
2026-02-23 20:31:32 +0300

    test

---
+++ cmd/main.go
@@ -0,0 +1,35 @@
+package main
+
+import (
+	"log"
+	"net/http"
+	"os"
+
+	"github.com/gorilla/mux"
+	"github.com/joho/godotenv"
+)
+
+func homeHandler(w http.ResponseWriter, r *http.Request) {
+	w.WriteHeader(http.StatusOK)
+	w.Write([]byte("Welcome to Energy Monitoring System"))
+}
+
+func main() {
+
+	if err := godotenv.Load(); err != nil {
+		log.Println("No .env file found, using system environment variables")
+	}
+	log.Println("Environment variables loaded successfully")
+
+	r := mux.NewRouter()
+	r.HandleFunc("/", homeHandler).Methods("GET")
+
+	port := os.Getenv("PORT")
+
+	if port == "" {
+		port = "8080"
+	}
+
+	log.Printf("Server starting on port %s", port)
+	log.Fatal(http.ListenAndServe(":"+port, r))
+}
---
+++ go.mod
@@ -0,0 +1,7 @@
+module energy-monitoring-system
+
+go 1.25.4
+
+require github.com/gorilla/mux v1.8.1
+
+require github.com/joho/godotenv v1.5.1
