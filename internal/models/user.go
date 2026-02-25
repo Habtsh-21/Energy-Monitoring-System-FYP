aef19fbc01949ac329b2f8829cce2783d72b1b76
Habtsh-21 <hworku901@gmail.com>
2026-02-25 16:59:23 +0300

    add methods for user model

--- internal/models/user.go
+++ internal/models/user.go
@@ -29,3 +29,33 @@ func(user *User) Create() error {
 	}
 	return nil
 }
+
+func(user *User) Update() error {
+	if err := db.DB.Save(user).Error; err != nil {
+		return err
+	}
+	return nil
+}
+
+func(user *User) Delete() error {
+	if err := db.DB.Delete(user).Error; err != nil {
+		return err
+	}
+	return nil
+}
+
+func GetUser(userId string) (*User, error) {
+	var user User
+	if err := db.DB.Where("ID = ?", userId).First(&user).Error; err != nil {
+		return nil, err
+	}
+	return &user, nil
+}
+
+func GetAllUser() ([]User, error) {
+	var users []User
+	if err := db.DB.Find(&users).Error; err != nil {
+		return nil, err
+	}
+	return users, nil
+}
